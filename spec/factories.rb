226fc8c6bcd1e3c92aa25d6c992a1e75075f7eb6
Arthur Rocha de Menezes <arocha@instructure.com>
2019-01-31 17:03:50 +0100

    Create factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,31 @@
 Factory.define do
+  define Employee do
+    company
+    department
+    user
+    name { Fake::RickAndMorty.character }
+    role { "employee" }
+
+    trait :admin do
+      role { "administrator" }
+    end
+  end
+
+  define User do
+    sequence :email do |n|
+      "user#{n}@test.com"
+    end
+
+    email
+    username { Fake::RickAndMorty.character }
+    employee
+  end
+
+  define Company do
+    name { Fake::GameOfThrones.house }
+  end
+
+  define Department do
+    name { Fake::GameOfThrones.house }
+  end
 end
